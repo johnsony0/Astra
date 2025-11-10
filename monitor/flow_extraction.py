f849bba18c39b60db0fc7b20950c6deca6407f49
Johnson Yang <johnsonyangwork@gmail.com>
2025-11-10 17:50:38 -0500

    monitor init

--- monitor/flow_extraction.py
+++ monitor/flow_extraction.py
@@ -2,7 +2,7 @@
 # monitoring data, when connection terminates we send it to the server's database and we use it later
 
 # prerequisites: windows Npcap for windows packet capture
-# know your network interface name ()
+# know your network interface name (probably "Wi-Fi" if on WiFi or "Eth1" if on ethernet or so on should check network configs)
 
 # CLI Command:
-# cicflowmeter -i "Wi-Fi" -u http://127.0.0.1:8000/write_flow 
+# cicflowmeter -i "Wi-Fi" -u http://127.0.0.1:8000/write_flow
